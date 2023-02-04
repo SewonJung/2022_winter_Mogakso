c0fe7f04277c1324a3edeb27f3584cf09e61c437
SewonJung <94056835+SewonJung@users.noreply.github.com>
2023-02-05 00:43:23 +0900

    Update Edmonds-Karp_Matrix.cpp

--- 3주차_NetworkFlow/Edmonds-Karp_Matrix.cpp
+++ 3주차_NetworkFlow/Edmonds-Karp_Matrix.cpp
@@ -1,17 +1,19 @@
-const int MAX_V = 1000;
-const int INF = 1<<30;
+const int MAX_V = 1000; //number of vertices
+const int INF = 1<<30; //set Infinity value which can't be value of flow (to get minimum residual capacity in path)
 
-int cap[MAX_V][MAX_V], flow[MAX_V][MAX_V];
+int cap[MAX_V][MAX_V], flow[MAX_V][MAX_V]; //using matrix to save capacity and flow
 
+//input : source, target(sink)
+//output : maximum flow of graph
 int maximum_flow(int s, int t){
-  memset(flow, 0, sizeof(flow));
-  int ret = 0;
+  int ret = 0; //return value
   while(1){
-    vector<int> parent(MAX_V, -1);
+    vector<int> parent(MAX_V, -1); //to backtrace augmenting path
     queue<int> q;
     parent[s] = s;
     q.push(s);
     
+    //using bfs to find augmenting path
     while(!q.empty() && parent[t] == -1){
       int cur = q.front();
       q.pop();
@@ -23,9 +25,11 @@ int maximum_flow(int s, int t){
         }
       }
     }
-    if (parent[t] == -1) break;
-    int new_flow = INF;
-      
+    
+    if (parent[t] == -1) break; //it means there's no additional augmeting path thus have to break while loop
+    
+    int new_flow = INF; //initialize flow value of augmenting path
+    
     for (int i = t; i != s; i = parent[i]) new_flow = min(cap[parent[i]][i] - flow[parent[i]][i], new_flow);
     for (int i = t; i != s; i = parent[i]){
       flow[parent[i]][i] += new_flow;
